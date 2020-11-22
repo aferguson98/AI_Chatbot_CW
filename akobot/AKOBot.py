18a5bbab3ec1f3fc6e42b8832af419f16d3f4321
aferguson98 <alexander_ferguson@hotmail.co.uk>
2020-11-22 20:41:20 +0000

    Added basic SpaCy engine class

--- akobot/AKOBot.py
+++ akobot/AKOBot.py
@@ -2,10 +2,33 @@
 AKOBot.py
 """
 
+import spacy
 
-class AKOBot:
-    pass
+
+class NLPEngine:
+    def __init__(self):
+        """
+        A basic NLP Engine that takes can process input text
+        """
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def process(self, input_text):
+        """
+        Takes in user input and uses SpaCy to process it
+
+        Parameters
+        ----------
+        input_text: str
+            The string the user has passed into the chatbot that needs to be
+            processed
+
+        Returns
+        -------
+        list
+            A list of tokens
+        """
+        return self.nlp(input_text)
 
 
 if __name__ == '__main__':
-    print("Hello, I'm AKO and I'm a kind of bot to help you travel smarter")
+    pass
