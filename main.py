3880ccb0ee426be40df43f9de8195819f7f44097
Omar <omar.mostafa@uea.ac.uk>
2021-01-22 12:54:15 +0000

    Adding port specification for deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from flask import Flask, jsonify, render_template, request
 
@@ -61,4 +62,5 @@ def process_user_input():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
